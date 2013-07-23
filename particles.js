8a742f79c67ce5e7913ea36f90ac6291676b4091
Alex S <bobylito@gmail.com>
2013-07-23 15:24:23 +0200

    New rendering : quadratic curve

--- particles.js
+++ particles.js
@@ -152,3 +152,74 @@ window.loop.animations.particleLasso =
   }
   return pGen;
 })();
+
+window.loop.animations.particleLasso2 =
+(function(){
+  var fields = [],
+      height = 0, 
+      width = 0;
+
+  var pGen = function createParticleField(init, createParticle, field, compositionMethod, color, size, eolCallback){
+    if(typeof(eolCallback)!="function"){
+      eolCallback = function(){};
+    }
+
+    var particles = [],
+        res = {
+          _init : function(n, w, h){
+            particles = init(n, w,h);
+          },
+          getParticleCount : function(){ return particles.length; },
+          render : function(context, width, height){
+            if(particles.length === 0) return ;
+            context.globalCompositeOperation = compositionMethod;
+            context.beginPath();
+            context.strokeStyle=color;
+            context.moveTo(~~(particles[0][0]), ~~(particles[0][1]));
+            for(var i = 1; i < particles.length; i++){
+              var p = particles[i];
+              context.quadraticCurveTo(p[0]- p[3] * 100, p[1] -p[4] * 100,~~p[0], ~~p[1]);
+            }
+            context.stroke();
+          },
+          animate : function(now, width, height){
+            for(var i = 0; i < particles.length; i++){
+              var p = particles[i],
+                  vd = field(p, particles),
+                  elapsedtime = (now - p[5])/1000;
+
+              p[0] = p[0] + p[3];
+              p[1] = p[1] + p[4];
+
+              if( p[0] > width+500 || 
+                  p[1] > height+500 || 
+                  p[0] < -500 || 
+                  p[1] < -500 ||
+                  p[2] < now ){
+                eolCallback(p)
+                particles.splice(i, 1);
+              }
+            }
+
+            return true;
+          }
+        }
+
+    res.create = function(nbParticules) {
+      var now = Date.now();
+      for(var i = 0; i<nbParticules; i++){
+        particles.push(createParticle(now, width, height));
+      }
+    };
+
+    fields.push(res);
+    return res;
+  }
+
+  pGen._init = function(w, h){
+    height = h;
+    width = w;
+    fields.forEach(function(e){e._init(Date.now(),w,h)})
+  }
+  return pGen;
+})();
